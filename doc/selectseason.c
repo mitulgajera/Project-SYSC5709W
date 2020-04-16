b35960e99ff87d85bdb1d6dce558128d6d53aca3
sanjanareddy8 <61893956+sanjanareddy8@users.noreply.github.com>
2020-04-15 20:14:33 -0400

    Add files via upload

---
+++ doc/selectseason.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<math.h>
+int main ()
+{
+  int season_num, destemp, meatemp;
+  printf ("Enter season_num: ");
+  scanf ("%d", &season_num);
+  switch (season_num)
+    {
+    case 1:
+      printf ("Summer\n");
+      break;
+    case 2:
+      printf ("winter\n");
+      break;
+    }
+  printf ("Enter the destemp: ");
+  scanf ("%d", &destemp);
+  printf ("Enter the meatemp: ");
+  scanf ("%d", &meatemp);
+  return 0;
+}
