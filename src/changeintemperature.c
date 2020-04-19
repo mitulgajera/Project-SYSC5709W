a2de851c5237217b8127195f63df41fc26c61ced
sanjanareddy8 <61893956+sanjanareddy8@users.noreply.github.com>
2020-04-18 21:55:56 -0400

    Add files via upload

---
+++ src/changeintemperature.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+char ans;
+int dtem= 30;
+while(1){
+printf("enter the number:");
+scanf("%d",&a);
+if(a>=16 && a<=35){
+printf("%d is measured temp\n", a);
+if(a==dtem)
+{
+printf("do u want change in temp:");
+scanf("%s", &ans);
+if(ans =='y')
+{
+    printf("enter the desired temp:");
+    scanf("%d",&b);
+    dtem=b;
+    while(1){
+if(dtem>=16 && dtem<=35){
+printf("%d is desired temp\n",dtem);
+printf("the desiredtemp is changed")
+;
+break;
+}
+else
+    {
+        printf("enter the desiredtemp again:");
+        scanf("%d",&dtem);
+    }
+    }
+
+}
+else(ans=='n');{
+    break;
+}
+}
+    else{
+        continue;
+    }
+}
+else
+{
+continue;
+}
+}
+}
+
+
+
