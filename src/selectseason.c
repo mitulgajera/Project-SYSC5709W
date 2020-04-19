b87c086dbd7fd3ea4c22bd0e7b567a8a694997bc
sanjanareddy8 <61893956+sanjanareddy8@users.noreply.github.com>
2020-04-18 21:50:35 -0400

    Update selectseason.c

--- src/selectseason.c
+++ src/selectseason.c
@@ -4,24 +4,25 @@
  *
  */
 #include <stdio.h>
-#include<math.h>
+#include<conio.h>
 int main()
 {
-int season_num,destemp,roomtemp;
-printf("Enter season_num: ");
-scanf("%d",&season_num);
-switch(season_num)
+int destemp,roomtemp;
+char season;
+printf("Enter season: ");
+scanf("%s",&season);
+switch(season)
 {
-    case 1:
+    case 's':
     printf("Summer\n");
 	       break;
-	case 2:
-	printf("winter\n");
+    case 'w':
+    printf("winter\n");
 	       break;
+}
 printf("Enter the destemp: ");
 scanf("%d",&destemp);
-printf("Enter the meatemp: ");
+printf("Enter the roomtemp: ");
 scanf("%d", &roomtemp);
 return 0;
-}
 }
