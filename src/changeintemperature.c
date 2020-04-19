9b32da0669aa9478244fcb609beff0d546c7f617
sanjanareddy8 <61893956+sanjanareddy8@users.noreply.github.com>
2020-04-18 22:48:20 -0400

    Update changeintemperature.c

--- src/changeintemperature.c
+++ src/changeintemperature.c
@@ -16,7 +16,7 @@ scanf("%d",&a);
 if(a>=16 && a<=35){
 printf("%d is roomtemp\n", a);
 if(a==dtem)
-/** One cycle is completed**/
+/** Cycle is completed**/
 {
 
 printf("Do you want change in temp: Enter \n 'y' for Yes \n 'n' for No\n");
