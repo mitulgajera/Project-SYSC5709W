9fbd2cfee5785010ad30afb10985496a82f1822d
sanjanareddy8 <61893956+sanjanareddy8@users.noreply.github.com>
2020-04-19 22:29:10 -0400

    Update changeintemperature.c

--- src/changeintemperature.c
+++ src/changeintemperature.c
@@ -7,15 +7,15 @@
 #include<conio.h>
 int main()
 {
-int a,b;
+int dtemp,roomtemp;
 char ans;
-int dtem= 30;
+int dtemp= 30;
 while(1){
 printf("Please enter the roomtemp:");
-scanf("%d",&a);
-if(a>=16 && a<=35){
-printf("%d is roomtemp\n", a);
-if(a==dtem)
+scanf("%d",&roomtemp);
+if(roomtemp>=16 && roomtemp<=35){
+printf("%d is roomtemp\n", roomtemp);
+if(roomtemp==dtemp)
 /** Cycle is completed that is dtemp=roomtemp**/
 {
 
@@ -24,21 +24,20 @@ scanf("%s", &ans);
 if(ans =='y')
 {
     printf("Please enter the desired temp:");
-    scanf("%d",&b);
-    dtem=b;
+    scanf("%d",&dtemp);
     /*** Validating input ***/
     while(1)
     {
-if(dtem>=16 && dtem<=35)
+if(dtemp>=16 && dtemp<=35)
 {
-printf("%d is desired temp\n",dtem);
+printf("%d is desired temp\n",dtemp);
 printf("The desiredtemp is changed"); /*** Change in temperature***/
 break;
 }
 else
     {
         printf("Please enter the desiredtemp again:");
-        scanf("%d",&dtem);
+        scanf("%d",&dtemp);
     }
     }
 
