d7990d1403b2607aabd6e5da2a10af48e28f06f2
mitulgajera <61294998+mitulgajera@users.noreply.github.com>
2020-04-24 19:22:23 -0400

    Add files via upload

---
+++ include/checkTemperature.h
@@ -0,0 +1,6 @@
+#ifndef CHECKTEMPERATURE_H_INCLUDED
+#define CHECKTEMPERATURE_H_INCLUDED
+
+int checkTemperature(int dt, int rt)
+
+#endif // CHECKTEMPERATURE_H_INCLUDED
---
+++ include/startCooling.h
@@ -0,0 +1,6 @@
+#ifndef STARTCOOLING_H_INCLUDED
+#define STARTCOOLING_H_INCLUDED
+
+void startCooling(int, int)
+
+#endif // STARTCOOLING_H_INCLUDED
